a9341657a1c7d76a561e260429c18cbab2483c1b
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-02-08 10:34:14 +0530

    Add files via upload

---
+++ testing/src/main/java/testclass/Testclass.java
@@ -0,0 +1,24 @@
+package testclass;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class Testclass {
+	String name;
+    public String display()
+    {
+    	return this.name;
+    }
+	public static void main(String[] args) {
+	       Testclass a1=new Testclass();
+	       a1.name="ganesh";
+	       PrintStream myOutput=new PrintStream(new FileOutputStream(FileDescriptor.out));
+	       myOutput.print("Name:"+a1.name);
+	       System.out.print(a1.name);
+           System.out.format(a1.name);
+           System.err.print(a1.name);
+           System.err.print(a1.display());
+	}
+
+}
